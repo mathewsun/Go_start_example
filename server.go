4a6440bb4090c98af8a60d09e5f6d9b0c5a914ae
matthew <mathewsun@rambler.ru>
2023-02-03 17:06:02 +0300

    init

---
+++ server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type msg string
+
+var counter int
+
+func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	//fmt.Fprint(resp, m)
+	counter = counter + 1
+	fmt.Fprint(resp, counter)
+
+}
+
+func main() {
+	msgHandler := msg("Hello from Web Server in Go 8")
+	fmt.Println("Server is listening...")
+	http.ListenAndServe("localhost:8181", msgHandler)
+}
